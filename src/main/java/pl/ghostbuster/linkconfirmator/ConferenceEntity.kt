e2460f1a20615478d564e8700f98c8600ebff31f
ghostbuster <kghost0@gmail.com>
2017-04-07 17:42:25 +0200

    save conference with participants to repository

--- src/main/java/pl/ghostbuster/linkconfirmator/ConferenceEntity.kt
+++ src/main/java/pl/ghostbuster/linkconfirmator/ConferenceEntity.kt
@@ -4,7 +4,7 @@ import javax.persistence.*
 
 @Entity
 data class ConferenceEntity(
-        @OneToMany(cascade = arrayOf(CascadeType.ALL))
+        @OneToMany(cascade = arrayOf(CascadeType.ALL), fetch = FetchType.EAGER)
         var participants: List<Participant> = emptyList(),
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         var id: Long = 0
--- src/test/java/pl/ghostbuster/linkconfirmator/NewConferenceControllerTest.kt
+++ src/test/java/pl/ghostbuster/linkconfirmator/NewConferenceControllerTest.kt
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -27,6 +29,8 @@ class NewConferenceControllerTest {
     @Autowired
     private lateinit var webApplicationContext: WebApplicationContext
     private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var conferenceRepository: ConferenceRepository
 
     @Before
     fun setUp() {
@@ -42,6 +46,22 @@ class NewConferenceControllerTest {
                     val actual = Jsoup.parse(it.response.contentAsString).toString()
                     val expected = Jsoup.parse(stringFromFile("new_conference_rendered.html")).toString()
                     assertThat(actual).isEqualTo(expected)
+                }
+    }
+
+    @Test
+    fun shouldSaveConferenceWithParticipantsToRepository() {
+        mockMvc.perform(post("/conference")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("participantsEmails", "test@test.github.pl")
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andDo {
+                    val conferences = conferenceRepository.findAll()
+                    assertThat(conferences).hasSize(1)
+                    val participants = conferences.first().participants
+                    assertThat(participants).hasSize(1)
+                    assertThat(participants.first().email).isEqualTo("test@test.github.pl")
                 }
     }
 
