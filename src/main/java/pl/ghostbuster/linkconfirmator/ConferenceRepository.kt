c8a0e62b371d540e9bf9cd5e4c361f18ae7da437
ghostbuster <kghost0@gmail.com>
2017-04-09 14:14:04 +0200

    Change conference repository to use UUID instead of long

--- src/main/java/pl/ghostbuster/linkconfirmator/ConferenceRepository.kt
+++ src/main/java/pl/ghostbuster/linkconfirmator/ConferenceRepository.kt
@@ -1,5 +1,6 @@
 package pl.ghostbuster.linkconfirmator
 
 import org.springframework.data.repository.CrudRepository
+import java.util.*
 
-interface ConferenceRepository : CrudRepository<ConferenceEntity, Long>
+interface ConferenceRepository : CrudRepository<ConferenceEntity, UUID>
