35cb86148e313a16c087e0af98de709e2584f1a5
ghostbuster <kghost0@gmail.com>
2017-04-09 11:45:02 +0200

    Refactor conference submission view test

---
+++ src/test/java/pl/ghostbuster/linkconfirmator/ViewTests.kt
@@ -0,0 +1,22 @@
+package pl.ghostbuster.linkconfirmator
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.thymeleaf.testing.templateengine.engine.TestExecutor
+
+class ViewTests {
+
+    @Test
+    fun `should render new_conference_submitted`() {
+        val executor = TestExecutor()
+        executor.execute("classpath:new_conference_submitted.thtest")
+        assertTrue(executor.isAllOK)
+    }
+
+    @Test
+    fun `should render new_conference_submission`() {
+        val executor = TestExecutor()
+        executor.execute("classpath:new_conference_submission.thtest")
+        assertTrue(executor.isAllOK)
+    }
+}
