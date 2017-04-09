a115dd2f49fe7150780d23f741b26116cd0ba1e7
ghostbuster <kghost0@gmail.com>
2017-04-09 13:04:46 +0200

    Improve integration tests

--- src/test/java/pl/ghostbuster/linkconfirmator/JpaIntegrationTest.kt
+++ src/test/java/pl/ghostbuster/linkconfirmator/JpaIntegrationTest.kt
@@ -6,7 +6,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
@@ -26,16 +25,28 @@ class JpaIntegrationTest {
     @Test
     fun `should save conference entity to repository`() {
         conferenceRepository.save(ConferenceEntity(participants = emptyList()))
+        assertThat(conferenceRepository.count()).isEqualTo(1)
     }
 
     @Test
     fun `should save conference entity with participant`() {
         conferenceRepository.save(ConferenceEntity(participants = listOf(Participant(email = "test@email"))))
+        assertThat(conferenceRepository.count()).isEqualTo(1)
+        val participant = conferenceRepository.findAll().first().participants.first()
+        assertThat(participant.email).isEqualTo("test@email")
     }
 
-    @EnableAutoConfiguration
-    @ComponentScan
+    @Test
+    fun `should save two conferences entity`() {
+        conferenceRepository.save(ConferenceEntity(participants = listOf(Participant(email = "first@email"))))
+        conferenceRepository.save(ConferenceEntity(participants = listOf(Participant(email = "second@email"))))
+
+        val participants = conferenceRepository.findAll().flatMap { it.participants }
+        assertThat(participants.map { it.email }).contains("first@email", "second@email")
+    }
+
     @Configuration
+    @EnableAutoConfiguration
     open class TestConfiguration
 
     @After
